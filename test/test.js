8d93dd9046d3af7e48108cd038d49de20cd9e4b4
Tony Pujals <tony@subfuzion.com>
2015-03-02 14:15:11 -0800

    added tests, pubtool command

---
+++ test/test.js
@@ -0,0 +1,37 @@
+var assert = require('assert');
+
+
+var notImplementedError = new Error('not implemented');
+
+it ('should load publish manifest', function() {
+ throw notImplementedError;
+});
+
+it ('should clone the manifest packages', function() {
+  throw notImplementedError;
+});
+
+
+it ('should clone the manifest packages', function() {
+  throw notImplementedError;
+});
+
+
+it ('should get the latest tags for each package', function() {
+  throw notImplementedError;
+});
+
+
+it ('should bump version and tag for updated packages', function() {
+  throw notImplementedError;
+});
+
+
+it ('should commit and push updated packages', function() {
+  throw notImplementedError;
+});
+
+
+it ('should publish updated packages', function() {
+  throw notImplementedError;
+});

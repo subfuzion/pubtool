f57348a7ec9adfa9743a2d5e434ceb323e5e3cb4
Tony Pujals <tony@subfuzion.com>
2015-03-02 16:21:36 -0800

    load package manifest

--- index.js
+++ index.js
@@ -1,1 +1,77 @@
-console.log('hello')
+var yaml  = require('js-yaml'),
+    async = require('async'),
+    git   = require('nodegit'),
+    fs    = require('fs'),
+    path  = require('path');
+
+var sourcePackagesFile = 'source-packages';
+
+module.exports = PubTool;
+
+function PubTool() {
+}
+
+PubTool.prototype.loadSourceManifest = function (dir) {
+  var manifest = loadManifest(dir, sourcePackagesFile);
+  this.manifest = manifest;
+  return manifest;
+};
+
+PubTool.prototype.clonePackages = function (dest, callback) {
+  var packageUrls = [];
+
+  Object.keys(this.manifest).forEach(function (pkg) {
+    packageUrls.push(this.manifest[pkg]);
+  }.bind(this));
+
+  async.eachSeries(packageUrls, function (url, cb) {
+    git.Clone.clone(url, dest, {
+      remoteCallbacks: {
+        credentials: function (url, userName) {
+          console.log(url, userName);
+          return Git.Cred.userpassPlaintextNew(process.env.GITHUB_TOKEN, '');
+        }
+      }
+    })
+    .then(function (repo) {
+      cb()
+    })
+    .catch(function (err) {
+      cb(err);
+    });
+  }, callback);
+};
+
+function loadManifest(dir, filename) {
+  var filepath = path.join(dir, filename);
+  var tempfile, finalpath, contents, doc;
+
+  if (!fs.existsSync(filepath)) {
+    if (!path.extname(filepath)) {
+      tempfile = filepath + '.yaml';
+      if (fs.existsSync(tempfile)) {
+        finalpath = tempfile;
+      } else {
+        tempfile = filepath + '.yml';
+        if (fs.existsSync(tempfile)) {
+          finalpath = tempfile;
+        } else {
+          tempfile = filepath + '.json';
+          if (fs.existsSync(tempfile)) {
+            finalpath = tempfile
+          }
+        }
+      }
+    }
+  } else {
+    finalpath = filepath;
+  }
+
+  if (!finalpath) {
+    throw new Error('file not found: ' + filepath);
+  }
+
+  contents = fs.readFileSync(finalpath, {encoding: 'utf8'});
+  doc = yaml.safeLoad(contents);
+  return doc;
+}
--- test/test.js
+++ test/test.js
@@ -1,37 +1,61 @@
-var assert = require('assert');
+var assert = require('assert'),
+    fsx = require('fs-extra'),
+    path = require('path'),
+    PubTool = require('..');
 
+var sampleDir = path.join(__dirname, 'sample');
+var clonePath = path.join(__dirname, 'tmp');
 
 var notImplementedError = new Error('not implemented');
 
-it ('should load publish manifest', function() {
- throw notImplementedError;
-});
+before(function() {
+  if (fsx.existsSync(clonePath)) {
+    fsx.removeSync(clonePath);
+  }
 
-it ('should clone the manifest packages', function() {
-  throw notImplementedError;
-});
+  //fsx.ensureDirSync(clonePath);
 
-
-it ('should clone the manifest packages', function() {
-  throw notImplementedError;
 });
 
 
-it ('should get the latest tags for each package', function() {
-  throw notImplementedError;
+describe('tests', function() {
+  var pubtool = new PubTool();
+
+  it ('should load source packages manifest', function() {
+    pubtool.loadSourceManifest(sampleDir);
+
+    assert(typeof pubtool.manifest === 'object');
+    assert.equal(pubtool.manifest.aqua, 'https://github.com/aquajs/aqua.git');
+  });
+
+
+  it ('should clone the manifest packages', function(done) {
+    pubtool.clonePackages(clonePath, done);
+  });
+
+
+  it ('should clone the manifest packages', function() {
+    throw notImplementedError;
+  });
+
+
+  it ('should get the latest tags for each package', function() {
+    throw notImplementedError;
+  });
+
+
+  it ('should bump version and tag for updated packages', function() {
+    throw notImplementedError;
+  });
+
+
+  it ('should commit and push updated packages', function() {
+    throw notImplementedError;
+  });
+
+
+  it ('should publish updated packages', function() {
+    throw notImplementedError;
+  });
 });
 
-
-it ('should bump version and tag for updated packages', function() {
-  throw notImplementedError;
-});
-
-
-it ('should commit and push updated packages', function() {
-  throw notImplementedError;
-});
-
-
-it ('should publish updated packages', function() {
-  throw notImplementedError;
-});
